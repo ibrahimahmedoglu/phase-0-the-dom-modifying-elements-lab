9c52a3c77d1acd9eb5963d1cda85be6962e25fc8
ibrahimahmedoglu <ibrahim.ahmedoglu@bahcesehir.edu.tr>
2023-06-17 15:17:31 +0300

    Completed Assignment

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Write your code here!
+const mainElement = document.querySelector("main#main");
+  mainElement.remove();
+
+  
+  const newHeader = document.createElement("h1");
+
+  
+  const namee = "YOUR-NAME"; // Replace YOUR-NAME with your actual name
+
+  // Assign the text and id to the new <h1> element
+  newHeader.innerHTML = `${namee} is the champion`;
+  newHeader.id = "victory";
+
+  
+
+  document.body.appendChild(newHeader);
